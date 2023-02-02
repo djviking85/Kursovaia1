b033efed241b30974f3966df676c64f4a123a952
Viking <djviking@yandex.ru>
2023-02-02 21:47:01 +0300

    Курсовая 1

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Employee rabotnik = new Employee("Иван", "Иванович", "Иванов", 1, 10000);
-     //   System.out.printf(" ФИО %s %s %s, номер отдела %d, с зарплатой %d%n",
-     //           rabotnik.getFirstNameEmployer(),
-     //           rabotnik.getOtchestvoEmployer(),
-      //          rabotnik.getSecondNameEmployer(),
-    //           Arrays.toString(rabotnik.getNumbersOtdels),
-     //           rabotnik.getZarplataEmployers());
+       System.out.printf(" ФИО %s %s %s, номер отдела %s, с зарплатой %d%n",
+                rabotnik.getFirstNameEmployer(),
+                rabotnik.getOtchestvoEmployer(),
+               rabotnik.getSecondNameEmployer(),
+               Arrays.toString(rabotnik.getNumbersOtdels),
+                rabotnik.getZarplataEmployers());
+       //
         System.out.println( ""+rabotnik.getFirstNameEmployer() +""+rabotnik.getOtchestvoEmployer()+""+rabotnik.getSecondNameEmployer()
         + Arrays.toString(rabotnik.getNumbersOtdels) + ""+  rabotnik.getZarplataEmployers() );
     }
