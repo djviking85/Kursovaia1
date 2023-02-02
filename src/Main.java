317027b7583a2360a65a663e5d3bbf80b9f324c7
Viking <djviking@yandex.ru>
2023-02-02 20:27:34 +0300

    Курсовая 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Employee Ivanov = new Employee();
-
+        Employee rabotnik = new Employee("Иван", "Иванович", "Иванов", 1, 10000);
+        System.out.printf(" ФИО %s %s %s, номер отдела %d, с зарплатой %d%n",
+                rabotnik.getFirstNameEmployer(),
+                rabotnik.getOtchestvoEmployer(),
+                rabotnik.getSecondNameEmployer(),
+                rabotnik.getNumbersOtdels,
+                rabotnik.getZarplataEmployers());
     }
 
 }
